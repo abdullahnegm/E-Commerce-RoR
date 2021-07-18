98c8ff00457ea2874f77b00b2cc20fc78a262a94
abdullahnegm <abdullahahmed1997@hotmail.com>
2021-07-18 16:41:44 +0200

    All user controller actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
     before_action :item_params, only: [:create, :update]
+    before_action :get_item, only: [:destroy, :update, :show]
 
     def index
         @items = Item.all
@@ -7,37 +8,36 @@ class ItemsController < ApplicationController
     end
 
     def show
-        @item = Item.find( params[:id].to_i )
         render json: @item
-    rescue
-        render json: {error: "Item not found"}, status: 404
     end
 
     def create
         @item = Item.new(item_params)
         @item.save ? 
-            (render json: @item, response: "Item Created Successfully") : 
-            (render json: { errors: @item.errors })
+            (render json: @item, response: "Item Created Successfully", status: :created) : 
+            (render json: { errors: @item.errors }, status: :bad_request)
     end
 
     def update
-        @item = Item.find( params[:id].to_i )
         @item.update(item_params) ? 
-            (render json: @item, response: "Item Updated Successfully") : 
-            (render json: { errors: @item.errors })
+            (render json: @item, response: "Item Updated Successfully", status: :created) : 
+            (render json: { errors: @item.errors }, status: :bad_request)
     end
 
     def destroy
-        @item = Item.find( params[:id].to_i )
         @item.destroy
-        render json: {response: "Item deleted successfully"}
-    rescue
-        render json: {error: "Item not found"}, status: 404
+        render json: {response: "Item deleted successfully", user: @user}
     end
 
     private
     def item_params
         params.permit(:title, :price, :dis_price, :description, :picture)
+    end
+
+    def get_item
+        @item = Item.find( params[:id].to_i )
+    rescue
+        render json: {error: "Item not found"}, status: :not_found
     end
 
 end
