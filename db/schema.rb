aef8ce41ff7af9da15f0f7d47368a684af578fcb
abdullahnegm <abdullahahmed1997@hotmail.com>
2021-07-18 16:17:55 +0200

    User setup [Controller, Model, Migration]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_183954) do
+ActiveRecord::Schema.define(version: 2021_07_18_141235) do
 
   create_table "items", force: :cascade do |t|
     t.string "title", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_07_17_183954) do
     t.string "slug"
     t.string "description", null: false
     t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
